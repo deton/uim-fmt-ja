7bd7a76008ab182374b92ed43ce378bf555c170e
KIHARA Hideto <deton@m1.interq.or.jp>
2011-12-05 20:15:55 +0900

    Change not to use readlink for FreeBSD.

--- uim-fmt-ja.sh
+++ uim-fmt-ja.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
-scmfile=$(readlink -f $(dirname $0)/uim-fmt-ja.scm)
+dir=$(cd "$(dirname $0)"; pwd)
+# XXX: use realpath or readlink -f
+scmfile="$dir/uim-fmt-ja.scm"
 if [ ! -t 0 ]; then	# data from stdin
-	nkf -e | uim-sh $scmfile
+	nkf -e | uim-sh "$scmfile"
 else
 	for f
 	do
-		if [ -f $f ]; then
-			nkf -e $f | uim-sh $scmfile
+		if [ -f "$f" ]; then
+			nkf -e "$f" | uim-sh "$scmfile"
 		fi
 	done
 fi
