09179bf5eb1d64bbe23dcc1a0c0648714038a174
KIHARA Hideto <deton@m1.interq.or.jp>
2011-12-11 14:19:39 +0900

    Support command line options for command line tools.

--- tools/uim-fmt-ja.scm
+++ tools/uim-fmt-ja.scm
@@ -6,5 +6,23 @@
       (read-input (cons ch res)))))
 
 (define (main args)
-  (let ((str (list->string (reverse (read-input '())))))
-    (display (fmt-ja-str str))))
+  (define (parse-num arg)
+    (and (> (string-length arg) 0)
+         (string->number arg)))
+  (let ((foldonly? (string=? (list-ref args 1) "t"))
+        (indchange? (string=? (list-ref args 2) "t"))
+        (goal (parse-num (list-ref args 3)))
+        (max (parse-num (list-ref args 4)))
+        (tab (parse-num (list-ref args 5))))
+    (set! fmt-ja-fold-only foldonly?)
+    (set! fmt-ja-new-paragraph-by-indent-change indchange?)
+    (if tab (set! fmt-ja-tab-width tab))
+    (if goal (set! fmt-ja-goal-width goal))
+    (if max (set! fmt-ja-max-width max))
+    (if (<= fmt-ja-goal-width fmt-ja-max-width)
+      (let ((str (list->string (reverse (read-input '())))))
+        (display (fmt-ja-str str)))
+      (display
+        (format "ERROR: max-width(~d) < goal-width(~d)\n"
+          fmt-ja-max-width
+          fmt-ja-goal-width)))))
--- tools/uim-fmt-ja.sh
+++ tools/uim-fmt-ja.sh
@@ -2,13 +2,28 @@
 dir=$(cd "$(dirname $0)"; pwd)
 # XXX: use realpath or readlink -f
 scmfile="$dir/uim-fmt-ja.scm"
+
+foldonly=f
+indchange=f
+while getopts fg:im:t: f
+do
+	case $f in
+	f) foldonly=t;;
+	g) goal=$OPTARG;;
+	i) indchange=t;;
+	m) max=$OPTARG;;
+	t) tab=$OPTARG;;
+	esac
+done
+shift `expr $OPTIND - 1`
+
 if [ ! -t 0 ]; then	# data from stdin
-	nkf -e | uim-sh "$scmfile"
+	nkf -e | uim-sh "$scmfile" "$foldonly" "$indchange" "$goal" "$max" "$tab"
 else
 	for f
 	do
 		if [ -f "$f" ]; then
-			nkf -e "$f" | uim-sh "$scmfile"
+			nkf -e "$f" | uim-sh "$scmfile" "$foldonly" "$indchange" "$goal" "$max" "$tab"
 		fi
 	done
 fi
