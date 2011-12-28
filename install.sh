14fd65c6cce6a4411dfd6d94b44dcfcd8a4d87be
KIHARA Hideto <deton@m1.interq.or.jp>
2011-12-28 17:57:55 +0900

    Change to use $srcdir to install from another directory.

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+srcdir=$(dirname $0)
 scmdir=$(pkg-config --variable=uim_scmdir uim)
 pixmapsdir=$(pkg-config --variable=uim_datadir uim)/pixmaps
-cp fmt-ja.scm fmt-ja-custom.scm "$scmdir"
-cp pixmaps/fmt-ja.png pixmaps/fmt-ja_dark_background.png "$pixmapsdir"
-cp pixmaps/fmt-ja.svg pixmaps/fmt-ja_dark_background.svg "$pixmapsdir"
+cp "$srcdir/fmt-ja.scm" "$srcdir/fmt-ja-custom.scm" "$scmdir"
+cp "$srcdir/pixmaps/fmt-ja.png" "$srcdir/pixmaps/fmt-ja_dark_background.png" "$pixmapsdir"
+cp "$srcdir/pixmaps/fmt-ja.svg" "$srcdir/pixmaps/fmt-ja_dark_background.svg" "$pixmapsdir"
 uim-module-manager --register fmt-ja
